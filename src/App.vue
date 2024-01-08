0f6e65be753cdb178d0eb8325a3009f840c4aed8
HuanXiang <huanxiangspace@qq.com>
2024-01-08 23:15:10 +0800

    create validator

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import CreateValidator from "./components/CreateValidator.vue";
+
+const nav = ref("cv");
+
+const setNav = (navType: string) => {
+  nav.value = navType;
+};
+</script>
+
+<template>
+  <div class="app">
+    <div class="nav">
+      <div class="item" @click="setNav('cv')">Create Validator</div>
+    </div>
+    <div class="main">
+      <div class="content">
+        <CreateValidator v-if="nav === 'cv'" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+}
+.nav {
+  width: 180px;
+  text-align: center;
+  background: #f1f1f1;
+  .item {
+    padding: 16px;
+    cursor: pointer;
+    &:hover {
+      background: #000;
+      color: #fff;
+    }
+  }
+}
+.main {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  overflow: auto;
+  text-align: center;
+  .content {
+    padding: 20px;
+  }
+}
+</style>
---
+++ src/components/CreateValidator.vue
@@ -0,0 +1,371 @@
+<script setup lang="ts">
+import { MsgCreateValidator } from "cosmjs-types/cosmos/staking/v1beta1/tx";
+import { TxBody, AuthInfo, TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import { encodePubkey } from "@cosmjs/proto-signing";
+import { Uint32 } from "@cosmjs/math";
+import {
+  toBech32,
+  fromBech32,
+  toBase64,
+  fromBase64,
+  toHex,
+} from "@cosmjs/encoding";
+import { Window, Keplr, AccountData, BroadcastMode } from "@keplr-wallet/types";
+import { computed, onMounted, reactive, ref, watch } from "vue";
+import Long from "long";
+
+let keplr: Keplr;
+let signer: AccountData;
+
+const isKeplrReady = ref(false);
+const fm = reactive({
+  description: {
+    moniker: "A Node",
+    identity: "ABCDEFGH12345678",
+    website: "https://abcd.com/",
+    details: "Something about description of this node.",
+    securityContact: "support@abcd.com or WeChat@abcd",
+  },
+  commission: {
+    maxChangeRate: "0.010000000000000000",
+    maxRate: "0.200000000000000000",
+    rate: "0.100000000000000000",
+  },
+  pubkey: "oxJ4lDdSSNHHhQ776UKdMIIhwsaD8YU3v4eLxQMP84s=",
+  memo: "",
+  minSelfDelegation: "1",
+  amount: "1000000LAKE",
+  delegatorAddress: "",
+  validatorAddress: "",
+  accountNumber: "12",
+  sequence: "0",
+  chainId: "iconlake-testnet-1",
+});
+const signature = ref("");
+let txBytes = ref(new Uint8Array());
+
+async function init() {
+  isKeplrReady.value = !!(window as Window).keplr;
+  if (!isKeplrReady.value) {
+    return;
+  }
+  keplr = (window as Window).keplr!;
+  await keplr?.enable(fm.chainId);
+  signer = (await keplr.getOfflineSigner(fm.chainId).getAccounts())[0];
+  fm.delegatorAddress = signer.address;
+  const res = await fetch(
+    `https://lcd.testnet.iconlake.com/cosmos/auth/v1beta1/accounts/${signer.address}`,
+  ).then((e) => e.json());
+  if (res.account) {
+    fm.accountNumber = res.account.account_number;
+    fm.sequence = res.account.sequence;
+  }
+}
+
+function toIntRate(rate: string) {
+  const r = rate.replace(/^0\./, "").substring(0, 18);
+  const fixLen = 18 - r.length;
+  return r.replace(/^0*/, "") + "0".repeat(fixLen);
+}
+
+async function sign() {
+  const createMsg = MsgCreateValidator.encode({
+    description: {
+      moniker: fm.description.moniker,
+      identity: fm.description.identity,
+      website: fm.description.website,
+      details: fm.description.details,
+      securityContact: fm.description.securityContact,
+    },
+    commission: {
+      maxChangeRate: toIntRate(fm.commission.maxChangeRate), // 0.01
+      maxRate: toIntRate(fm.commission.maxRate), // 0.2
+      rate: toIntRate(fm.commission.rate), // 0.1
+    },
+    minSelfDelegation: fm.minSelfDelegation,
+    delegatorAddress: fm.delegatorAddress,
+    validatorAddress: fm.validatorAddress,
+    pubkey: encodePubkey({
+      type: "tendermint/PubKeyEd25519",
+      value: fm.pubkey,
+    }),
+    value: {
+      amount: (parseInt(fm.amount) * 1000000).toString(),
+      denom: "ulake",
+    },
+  }).finish();
+
+  const bodyBytes = TxBody.encode({
+    messages: [
+      {
+        typeUrl: MsgCreateValidator.typeUrl,
+        value: createMsg,
+      },
+    ],
+    memo: fm.memo,
+    timeoutHeight: new Uint32(0).toBigInt(),
+    extensionOptions: [],
+    nonCriticalExtensionOptions: [],
+  }).finish();
+
+  const authInfoBytes = AuthInfo.encode({
+    signerInfos: [
+      {
+        publicKey: encodePubkey({
+          type: "tendermint/PubKeySecp256k1",
+          value: toBase64(signer.pubkey),
+        }),
+        modeInfo: {
+          single: {
+            mode: 1,
+          },
+        },
+        sequence: new Uint32(+fm.sequence).toBigInt(),
+      },
+    ],
+    fee: {
+      amount: [],
+      gasLimit: new Uint32(200000).toBigInt(),
+      payer: "",
+      granter: "",
+    },
+  }).finish();
+
+  const res = await keplr.signDirect(
+    fm.chainId,
+    signer.address,
+    {
+      bodyBytes,
+      authInfoBytes,
+      chainId: fm.chainId,
+      accountNumber: new Long(+fm.accountNumber),
+    },
+    {
+      preferNoSetFee: true,
+      preferNoSetMemo: true,
+      disableBalanceCheck: true,
+    },
+  );
+
+  signature.value = res.signature.signature;
+
+  txBytes.value = TxRaw.encode({
+    bodyBytes: bodyBytes,
+    authInfoBytes: authInfoBytes,
+    signatures: [fromBase64(signature.value)],
+  }).finish();
+}
+
+function isObject(v: any) {
+  return v.toString() === "[object Object]";
+}
+
+onMounted(() => {
+  init();
+});
+
+watch(() => fm.chainId, init);
+
+watch(
+  () => fm.delegatorAddress,
+  () => {
+    fm.validatorAddress = toBech32(
+      "iconlakevaloper",
+      fromBech32(fm.delegatorAddress).data,
+    );
+  },
+);
+
+const signedTX = computed(() => {
+  return JSON.stringify(
+    {
+      body: {
+        messages: [
+          {
+            "@type": "/cosmos.staking.v1beta1.MsgCreateValidator",
+            description: {
+              moniker: fm.description.moniker,
+              identity: fm.description.identity,
+              website: fm.description.website,
+              security_contact: fm.description.securityContact,
+              details: fm.description.details,
+            },
+            commission: {
+              rate: fm.commission.rate,
+              max_rate: fm.commission.maxRate,
+              max_change_rate: fm.commission.maxChangeRate,
+            },
+            min_self_delegation: fm.minSelfDelegation,
+            delegator_address: fm.delegatorAddress,
+            validator_address: fm.validatorAddress,
+            pubkey: {
+              "@type": "/cosmos.crypto.ed25519.PubKey",
+              key: fm.pubkey,
+            },
+            value: {
+              denom: "ulake",
+              amount: (parseInt(fm.amount) * 1000000).toString(),
+            },
+          },
+        ],
+        memo: fm.memo,
+        timeout_height: "0",
+        extension_options: [],
+        non_critical_extension_options: [],
+      },
+      auth_info: {
+        signer_infos: [
+          {
+            public_key: {
+              "@type": "/cosmos.crypto.secp256k1.PubKey",
+              key: signer ? toBase64(signer.pubkey) : "",
+            },
+            mode_info: {
+              single: {
+                mode: "SIGN_MODE_DIRECT",
+              },
+            },
+            sequence: "0",
+          },
+        ],
+        fee: {
+          amount: [],
+          gas_limit: "200000",
+          payer: "",
+          granter: "",
+        },
+        tip: null,
+      },
+      signatures: [signature.value],
+    },
+    undefined,
+    2,
+  );
+});
+
+const broadStatus = ref(0);
+const txHash = ref("");
+const txResult = ref("");
+
+async function broadcast() {
+  broadStatus.value = 1;
+  const txHashBytes = await keplr
+    .sendTx(fm.chainId, txBytes.value, "sync" as BroadcastMode)
+    .catch((e) => {
+      console.error(e);
+      broadStatus.value = 4;
+      txResult.value = e.message;
+    });
+  if (!txHashBytes) {
+    return;
+  }
+  txHash.value = toHex(txHashBytes);
+  broadStatus.value = 2;
+  const txRes = await fetch(
+    `https://lcd.testnet.iconlake.com/cosmos/tx/v1beta1/txs/${txHash.value}`,
+  )
+    .then((e) => e.json())
+    .catch((e) => {
+      console.error(e);
+      broadStatus.value = 4;
+      txResult.value = e.message;
+    });
+  if (!txRes) {
+    return;
+  }
+  broadStatus.value = 3;
+  txResult.value = JSON.stringify(txRes, undefined, 2);
+}
+</script>
+
+<template>
+  <div v-if="!isKeplrReady">Keplr is not installed.</div>
+  <div class="fm" v-else>
+    <div v-for="(v, k) in fm">
+      <div v-if="isObject(v)" class="group">
+        <h5>{{ k }}</h5>
+        <div class="flex" v-for="(vC, kC) in v">
+          <label>{{ kC }}</label>
+          <div>
+            <input type="text" v-model="fm[k][kC]" />
+            <div class="help" v-if="kC === 'identity'">
+              It can be used to verify identity with systems like Keybase or
+              UPort.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-else class="flex">
+        <label>{{ k }}</label>
+        <div>
+          <input type="text" v-model="fm[k]" />
+          <div class="help" v-if="k === 'pubkey'">
+            Get key by running `./iconlaked tendermint show-validator`.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="btns">
+      <button @click="sign">Sign</button>
+    </div>
+  </div>
+  <div v-if="isKeplrReady" class="rs">
+    <h4>Signed TX</h4>
+    <textarea cols="90" rows="66">{{ signedTX }}</textarea>
+    <div class="btns" v-if="broadStatus === 0 || broadStatus === 4">
+      <button @click="broadcast">Broadcast</button>
+    </div>
+    <div class="msg" v-else-if="broadStatus === 1">Broading</div>
+    <div class="msg" v-if="broadStatus === 2">TxHash: {{ txHash }}</div>
+    <div class="msg" v-if="broadStatus === 3">TxResult: {{ txResult }}</div>
+    <div class="msg" v-if="broadStatus === 4">Error: {{ txResult }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+body {
+  margin: 0;
+}
+
+.flex {
+  display: flex;
+  margin-top: 16px;
+}
+
+.group {
+  margin-bottom: 50px;
+}
+
+.btns {
+  margin-top: 50px;
+}
+
+.fm {
+  label,
+  h5 {
+    width: 220px;
+    text-align: right;
+    margin-right: 12px;
+  }
+
+  input {
+    width: 400px;
+  }
+}
+
+.help {
+  text-align: left;
+  font-size: 12px;
+}
+
+.rs {
+  padding-bottom: 50px;
+  h4 {
+    text-align: left;
+  }
+}
+
+.msg {
+  margin-top: 16px;
+}
+</style>
